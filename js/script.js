a368dc286838047fc8f1341510875dea5f6e9906
MartinP3 <martinpcitizen@gmail.com>
2022-01-30 14:06:36 +0100

    Question 1 answered

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+// Question 1
+const cat = {
+    complain() {
+        return "Meow!";
+    },
+}
+
+console.log(cat.complain());
+
+// Question 2
